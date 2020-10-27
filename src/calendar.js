2e2c9c22539d50c31b91c9ca59656ed080a8343b
Brianna <bnf8555@g.rit.edu>
2020-10-27 16:55:37 -0400

    moving/adding directories for organization

---
+++ src/calendar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+ReactDOM.render(
+    <React.StrictMode>
+        <head>
+            <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet" />
+            <link href="./css/calendar.css" rel="stylesheet" />
+        </head>
+        <body>
+
+        </body>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
