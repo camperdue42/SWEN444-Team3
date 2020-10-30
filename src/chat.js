3d4047258ca1472442ae638301348c22f7a440a8
JohnMcCarroll <jtm5356@rit.edu>
2020-10-30 12:42:53 -0400

    prototype chat page added

---
+++ src/chat.js
@@ -0,0 +1,100 @@
+import './chat.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// This Module defines the UI components of the Chat page
+
+class chat extends React.Component {
+    render() {
+        return (
+            <div>
+                <div>
+                    <Banner></Banner>
+                </div>
+                <div class="body">
+                    <Chat></Chat>
+                    <Menu></Menu>
+                </div>
+            </div>
+        );
+    }
+}
+
+class Banner extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+                        username : "Chet"
+                     };
+    }
+
+    render() {
+        return ( 
+            <div class="wrapper">
+                <button class="back"></button>
+                <button class="home"></button>
+                <span class="right">
+                    Hi, {this.state.username}!
+                </span>
+            </div>
+        );
+    }
+}
+
+class Chat extends React.Component {
+    render() {
+        return (
+            <div class="window">
+              <div class="chat">
+                  <div class="box">
+                        <textarea class="textarea" name="message" rows="5">
+                            Chat Away! :)
+                        </textarea>
+                        <button class="send-button">Send</button>
+                  </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+class Menu extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+                        common_facts : this.getCommonFacts()
+                    }
+
+    }
+
+    getCommonFacts() {
+        return (
+            <>
+            <div class="facts"> Likes Philidephia Eagles </div>
+            <div class="facts"> Likes Garage Rock </div>
+            </>
+                );
+    }
+
+    render() {
+        return (
+            <div class="window">
+              <div class="side-window">
+                  <div class="side-box">
+                    <div class="side-button-container">
+                        <button class="end-button"> End Conversation </button>
+                        <button class="report-button"></button>
+                    </div>
+                    <div class="facts-grid">
+                        <div class="facts"> Things you have in common: </div>
+                        {this.state.common_facts}
+                    </div>
+                  </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+
+export default chat;
