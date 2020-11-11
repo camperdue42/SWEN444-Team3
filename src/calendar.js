70d503133d059429d6bcf9f28c3b39a9584bfe67
Brianna <bnf8555@g.rit.edu>
2020-11-11 13:18:59 -0500

    Final version of modal

--- src/calendar.js
+++ src/calendar.js
@@ -2,7 +2,8 @@ import React from 'react';
 import Calendar from 'react-calendar';
 import './css/calendar.scss';
 import Banner from './banner';
-import ReactModal from 'react-modal';
+import Modal from 'react-bootstrap/Modal';
+import Button from "react-bootstrap/Button";
 
 class CalendarPage extends React.Component {
     constructor(props) {
@@ -15,10 +16,15 @@ class CalendarPage extends React.Component {
         this.tileClassName = this.tileClassName.bind(this);
         this.openModal = this.openModal.bind(this);
         this.onClickDay = this.onClickDay.bind(this);
+        this.closeModal = this.closeModal.bind(this);
     }
 
     openModal() {
         this.setState({modalIsOpen: true});
+    }
+
+    closeModal() {
+        this.setState({modalIsOpen: false});
     }
 
     onClickDay(value, event) {
@@ -38,18 +44,27 @@ class CalendarPage extends React.Component {
             <div>
                 <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet" />
                 <Banner />
-                <ReactModal
-                  isOpen={this.state.modalIsOpen}
-                  className='modal'
-                >
-                    <h1>{this.state.selectedDate}</h1>
-                    <p>Today I balanced some spreadsheets. It was just okay. Got some drinks with the boys.</p>
-                    <h2>Mood: <span></span></h2>
-                    <h2>Hours of sleep: 7</h2>
-                    <h2>Tagged: </h2>
+                <Modal show={this.state.modalIsOpen} onHide={this.closeModal} className="modal">
+                    <Modal.Header>
+                        <Modal.Title className="modalTitle">
+                            <h1>November 11, 2020</h1>
+                        </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <p>Today I balanced some spreadsheets. It was just okay. Got some drinks with the boys.</p>
+                        <h2>Mood:</h2>
+                        <button class='mood'></button>
+                        <div></div>
+                        <h2>Hours of sleep: <span>7</span></h2>
+                        <div></div>
+                        <h2>Tagged: </h2>
+                        <div className='tag'></div>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button onClick={this.closeModal} className='closeButton'>Close</Button>
+                    </Modal.Footer>
 
-
-                </ReactModal>
+                </Modal>
                 <Calendar className="react-calendar" tileClassName={this.tileClassName} onClickDay={this.onClickDay}/>
             </div>
 
