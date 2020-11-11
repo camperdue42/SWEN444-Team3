d264dd884c847e95cdbc6a8b773aa8950e7690c5
Brianna <bnf8555@g.rit.edu>
2020-11-11 13:43:54 -0500

    home page initial render in react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import './App.css';
 import React from 'react'
-import CalendarPage from "./calendar";
+import HomePage from "./home";
 
 function App() {
   return (
-    <CalendarPage />
+    <HomePage/>
   );
 }
 
